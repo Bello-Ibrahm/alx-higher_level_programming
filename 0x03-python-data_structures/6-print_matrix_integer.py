545f1e792d6786e565cd23f8ac6d32dccfef1d1f
an-nur-info-tech <wasiubello60@gmail.com>
2023-09-09 13:08:28 -0400

    Fix task 6 #1

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,4 +3,6 @@ def print_matrix_integer(matrix=[[]]):
     for idx in matrix:
         for ele in idx:
             print(ele, end=' ')
+            if(ele is not idx[len(idx)-1]):
+                print(" ", end="")
         print()
