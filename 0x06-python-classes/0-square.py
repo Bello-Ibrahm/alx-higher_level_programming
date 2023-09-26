bb3ae562f7c3b191418081d383e3a85db51b5976
an-nur-info-tech <wasiubello60@gmail.com>
2023-09-26 15:28:57 -0400

    Fix task 0 #7

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,4 +1,9 @@
 #!/usr/bin/python3
-"""Write an empty class Square that defines a square:"""
+"""Write an empty class Square that defines a square:
+You are not allowed to import any module
+"""
 class Square:
+    """An empty class Square that defines a square:
+    This take no attribute and does not return anything
+    """
     pass
