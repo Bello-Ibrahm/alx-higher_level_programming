8eb8a3a860aed68c3d206f41f0f73bd8e4c063c0
an-nur-info-tech <wasiubello60@gmail.com>
2023-09-26 08:13:28 -0400

    Initial commit with task 0-2

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+class Square:
+    """class Square that defines a square by:
+       -Private instance attribute: size
+       -Instantiation with size"""
+
+    def __init__(self, size):
+        """ Class Constructor"""
+        self.__size = size
