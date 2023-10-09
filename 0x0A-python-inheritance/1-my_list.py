1f54eeeff7cc763b0042017442279a2fd098f968
an-nur-info-tech <wasiubello60@gmail.com>
2023-10-09 10:02:48 -0400

    Fix checker error #1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -7,4 +7,6 @@ class MyList(list):
 
     def print_sorted(self):
         """ prints the list, but sorted (ascending sort) """
-        print(sorted(self))
+        temp = list(self)
+        temp.sort()
+        print(temp)
