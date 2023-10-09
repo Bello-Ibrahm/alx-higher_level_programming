5b6e05e6dd5e5e516338440e8854fdbd053a8c66
an-nur-info-tech <wasiubello60@gmail.com>
2023-10-09 10:23:20 -0400

    Add task 7-10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Write a class Square that inherits from Rectangle (9-rectangle.py):
+"""
+
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Class that defines a Square by inheritance of Rectangle class """
+
+    def __init__(self, size):
+        """ Constructor """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def area(self):
+        """ Area method through Super call """
+        return super().area()
