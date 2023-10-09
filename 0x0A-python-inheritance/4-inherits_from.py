7144e4894ca8a99c943e9e9079d8cfe82238394f
an-nur-info-tech <wasiubello60@gmail.com>
2023-10-09 12:53:23 -0400

    Fix error #2

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,5 +9,5 @@ def inherits_from(obj, a_class):
     """ returns True if the object is an instance of a class that
     inherited (directly or indirectly) from the specified class ;
     otherwise False """
-    result = isinstance(obj, a_class) and type(obj) is not a_class
+    result = isinstance(obj, a_class) and type(obj) != a_class
     return result
