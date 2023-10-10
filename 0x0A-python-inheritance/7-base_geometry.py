9b8ece364b9684488f68a15f90301af24c87a49c
an-nur-info-tech <wasiubello60@gmail.com>
2023-10-10 05:31:43 -0400

    Fix chechker

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -5,15 +5,10 @@
 class BaseGeometry:
     """ Geometry Class """
     def area(self):
-        """ Not Yet Implemented """
         raise Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """ Validates Value
-
-        name is assumed to be a string"""
-
-        if type(value) != int:
+        if type(value) is not int:
             msg = "{:s} must be an integer".format(name)
             raise TypeError(msg)
         if value <= 0:
