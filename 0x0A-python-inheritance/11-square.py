c23abd1f112afd21d0bb8e33c640c1a1f2c22303
an-nur-info-tech <wasiubello60@gmail.com>
2023-10-10 06:33:26 -0400

    Fix checker

--- 0x0A-python-inheritance/11-square.py
+++ 0x0A-python-inheritance/11-square.py
@@ -1,24 +1,27 @@
 #!/usr/bin/python3
 """Write a class Square that inherits
 from Rectangle (9-rectangle.py). (task based on 10-square.py).
-"""
 
-Rectangle = __import__('9-rectangle').Rectangle
+Rectangle = __import__('9-rectangle').Rectangle"""
 
 
 class Square(Rectangle):
     """ Class that defines a Square by inheritance of Rectangle class """
 
     def __init__(self, size):
-        """initializes Square
+        """initializes Square"""
 
-        Attributes:
-            self.__size: size of one side of the square
-
-        Args:
-            size: size of one side of the square
-
-        """
         super().__init__(size, size)
         super().integer_validator("size", size)
         self.__size = size
+
+
+class Rectangle(BaseGeometry):
+    """ Class to define object Rectangle from BaseGeometry inheritance """
+
+    def __init__(self, width, height):
+        """ Constructor """
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
