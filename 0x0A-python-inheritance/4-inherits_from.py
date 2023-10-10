9af10e4c7a864c8f4f11fffed5a24873d69187e6
an-nur-info-tech <wasiubello60@gmail.com>
2023-10-10 08:13:52 -0400

    Fix checker

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
-"""Class that inherits from list """
+"""checks if object is an instance of a class that
+inherited from the specified class or not
+"""
 
 
 def inherits_from(obj, a_class):
-    """function"""
-    result = isinstance(obj, a_class) and type(obj) is not a_class
-    return result
+    """Returns true if object is an instance of a class that inherited
+    (directly or indirectly) from the specified class; otherwise False
+    """
+    return (issubclass(type(obj), a_class) and type(obj) != a_class)
