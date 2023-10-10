e346ca72b47cd6e8591913f47b97774586ccc2e4
an-nur-info-tech <wasiubello60@gmail.com>
2023-10-10 08:07:04 -0400

    Fix checker

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+"""Comments"""
 
 
 def number_of_lines(filename=""):
     """ function that returns the number of lines of a text file """
 
-    with open(filename, "r", encoding="UTF-8") as f:
-        return len(list(f))
+    with open(filename, "w", encoding="utf-8") as f:
+        return f.write(text)
