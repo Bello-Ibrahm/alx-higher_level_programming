36a93c236499c5b0d4bf6abafdf4659374985564
an-nur-info-tech <wasiubello60@gmail.com>
2023-10-10 05:23:39 -0400

    Fix chechker

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -3,9 +3,8 @@
 
 
 class MyList(list):
-    """inherited class MyList"""
+    """inherited class"""
     def print_sorted(self):
-        """ prints the list, but sorted (ascending sort) """
         temp = list(self)
         temp.sort()
         print(temp)
