90970498729b2562013faff3fd028c45cf293e79
an-nur-info-tech <wasiubello60@gmail.com>
2023-10-10 08:46:58 -0400

    Fix error

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -2,13 +2,9 @@
 """Comments"""
 
 
-def read_lines(filename="", nb_lines=0):
-    """ function that reads n lines of a text file (UTF8) and prints it
-    to stdout"""
+def append_write(filename="", text=""):
+    """ function that appends a string at the end of a text file (UTF8)
+    and returns the number of characters added"""
 
-    with open(filename) as myfile:
-        lines = myfile.readlines()
-        if nb_lines <= 0 or nb_lines >= len(lines):
-            [print(i, end='') for i in lines]
-        else:
-            [print(lines[i], end='') for i in range(0, nb_lines)]
+    with open(filename, "a", encoding="utf-8") as myfile:
+        return myfile.write(text)
