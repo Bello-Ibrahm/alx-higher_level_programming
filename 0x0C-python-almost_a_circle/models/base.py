923e8df98ee754885af8186642a0420463f9e161
an-nur-info-tech <wasiubello60@gmail.com>
2023-10-15 05:07:44 -0400

    16-JSON string to file

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -27,3 +27,14 @@ class Base:
                 all(type(i) is dict for i in list_dictionaries)):
             raise TypeError("list_dictionaries must be a list of dictionaries")
         return (json.dumps(list_dictionaries))
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write JSON string representation of list_objs to file"""
+        file_name = cls.__name__ + ".json"
+        with open(file_name, "w") as jsonfile:
+            if list_objs is None:
+                jsonfile.write("[]")
+            else:
+                list_dicts = [obj.to_dictionary() for obj in list_objs]
+                jsonfile.write(Base.to_json_string(list_dicts))
