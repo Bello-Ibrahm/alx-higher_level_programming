2c1200195b43e9d60acd5aa23f0b8e4cc84abab0
Bello-Ibrahm <ibrahimbello1243@gmail.com>
2023-12-11 12:50:28 -0800

    Fix checker #2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,8 +1,4 @@
 #!/usr/bin/node
 const argc = process.argv.length;
 
-if (argc > 2) {
-  console.log('Argument' + (argc > 3 ? 's' : '') + ' found');
-} else {
-  console.log('No argument');
-}
+console.log(argc === 2 ? 'No argument' : argc === 3 ? 'Argument found' : 'Arguments found');
