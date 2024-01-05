5205a76d4f9603118b9ab556246cf669af78f1a8
Bello-Ibrahm <ibrahimbello1243@gmail.com>
2024-01-05 06:38:24 -0800

    Re run

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 # Script that takes in a URL, sends a request to that URL, and displays the size of the body of the response
-
-[ "$#" -eq 1 ] && curl -s "$1" | wc -c || echo "Usage: <./script>  <URL>"
+curl -s "$1" | wc -c
