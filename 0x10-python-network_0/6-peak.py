dfc6630a3c37e84b66f57bdbb58f072adc212c05
Bello-Ibrahm <ibrahimbello1243@gmail.com>
2024-01-06 07:55:15 -0800

    Task 6

---
+++ 0x10-python-network_0/6-peak.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+# Comment
+def find_peak(list_of_integers):
+    if not list_of_integers:
+        return None
+
+    low, high = 0, len(list_of_integers) - 1
+
+    while low < high:
+        mid = (low + high) // 2
+
+        # Compare the middle element with its neighbors
+        if list_of_integers[mid] > list_of_integers[mid + 1]:
+            # If mid ele > next one, peak might be on the left side
+            high = mid
+        else:
+            # If mid ele <= next one, a peak might be on the right side
+            low = mid + 1
+
+    # 'low' now points to a potential peak
+    return list_of_integers[low]
