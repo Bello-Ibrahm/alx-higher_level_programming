006e18bfb1419fcdd3d6173471db9863df8e4c4f
Bello-Ibrahm <ibrahimbello1243@gmail.com>
2024-01-30 00:27:17 -0800

    Task 2. Status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -2,10 +2,10 @@
 const req = require('request');
 const url = process.argv[2];
 
-req.get(url)
-  .then(res => {
-    console.log(`code: ${res.statusCode}`);
-  })
-  .catch(err => {
+req.get(url, 'utf-8', (err, content) => {
+  if (err) {
     console.log(err);
-  });
+  } else {
+    console.log(`code: ${content.statusCode}`);
+  }
+});
